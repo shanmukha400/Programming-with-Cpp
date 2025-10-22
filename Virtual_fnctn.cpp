c3c149916b1f1caf7efca38b614e55df0d735475
shanmukha400 <nadimpillishanmukha@gmail.com>
2025-10-22 08:20:28 +0530

    Create Virtual_fnctn.cpp
    Develop a C++ program that illustrates runtime polymorphism using virtual functions.

---
+++ Virtual_fnctn.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+ virtual void makeSound() {
+ cout << "Animal makes a sound" << endl;
+ }
+};
+class Dog : public Animal {
+public:
+ void makeSound() override {
+ cout << "Dog barks" << endl;
+ }
+};
+class Cat : public Animal {
+public:
+ void makeSound() override {
+ cout << "Cat meows" << endl;
+ }
+};
+int main()
+{
+ 
+ Animal* animalPtr;
+Dog d;
+ Cat c;
+animalPtr = &d;
+animalPtr->makeSound(); 
+animalPtr = &c;
+animalPtr->makeSound(); 
+return 0;
+}
+
